d27835d8ae2846d78ddbe3122e6cea445d5e2ca5
Sebastian <s.shing.smith@gmail.com>
2025-02-24 13:14:13 -0500

    added filtering channel videos, pull snippet data

--- ytmain.py
+++ ytmain.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
 
-# Sample Python code for youtube.channels.txt.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
 from __future__ import unicode_literals
 from werkzeug.utils import secure_filename
 import json
 import os
 import re
 from datetime import date
-import ffmpeg
 
 import yt_dlp as youtube_dl
 import googleapiclient.discovery
@@ -35,9 +30,7 @@ def singleVid(videoId, *args, **kwargs):
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     youtube = apiAccess()
-    startdate = kwargs.get("startdate", None)
-    enddate = kwargs.get("enddate", None)
-    filter =  kwargs.get("filter", ["replies","snippet"])
+    filter = kwargs.get("filter", ["replies", "snippet"])
     part = ','.join(filter)
     request = youtube.commentThreads().list(
         part=part,
@@ -48,16 +41,6 @@ def singleVid(videoId, *args, **kwargs):
         id=videoId
     )
     info = findtitle.execute()
-    datePublished = info["items"][0]["snippet"]["publishedAt"][:10]
-    datePublished = date.fromisoformat(datePublished)
-    if startdate is not None and startdate != "":
-        startdate = date.fromisoformat(startdate)
-        if datePublished < startdate:
-            return
-    if enddate is not None and enddate != "":
-        enddate = date.fromisoformat(enddate)
-        if datePublished > enddate:
-            return
     if info['items']:
         channel = info["items"][0]["snippet"]["channelTitle"]
         title = info["items"][0]["snippet"]["title"]
@@ -103,30 +86,92 @@ def stripUsers(filename):
         return names
 
 #return ids from uploads playlist
-def getVideosFromPlaylist(channelIds,vidNumber):
+def getVideosFromPlaylist(channelIds):
     youtube = apiAccess()
     ret = []
+    i=1
     for channelId in channelIds:
-        request = youtube.playlistItems().list(
-            part="contentDetails",
-            #maxresults dictates how many videos from the playlist
-            maxResults=vidNumber,
-            playlistId=channelIds[channelId]
-        )
-        response = request.execute()
-        for item in response['items']:
-            ret.append(item['contentDetails']['videoId'])
+        print(channelId + " - " +str(i)+ "/"+ str(len(channelIds)) )
+        i+=1
+        nextPageToken = ""
+        while nextPageToken is not None:
+            if nextPageToken == "":
+                nextPageToken = None
+            request = youtube.playlistItems().list(
+                part="contentDetails",
+                #maxresults dictates how many videos from the playlist
+                maxResults=50,
+                pageToken=nextPageToken,
+                playlistId=channelIds[channelId]
+            )
+            response = request.execute()
+            for item in response['items']:
+                ret.append(item['contentDetails']['videoId'])
+            nextPageToken = response.get('nextPageToken')
     return ret
 
+def filterVideos(videoId, *args, **kwargs):
+    youtube = apiAccess()
+    startdate = kwargs.get("startdate", None)
+    enddate = kwargs.get("enddate", None)
+    findtitle = youtube.videos().list(
+        part="snippet",
+        id=videoId
+    )
+    info = findtitle.execute()
+    datePublished = info["items"][0]["snippet"]["publishedAt"][:10]
+    datePublished = date.fromisoformat(datePublished)
+    if startdate is not None and startdate != "":
+        startdate = date.fromisoformat(startdate)
+        if datePublished < startdate:
+            return False
+    if enddate is not None and enddate != "":
+        enddate = date.fromisoformat(enddate)
+        if datePublished > enddate:
+            return False
+    with open("joy-info.txt", "a") as f:
+        f.write(json.dumps(info["items"][0]["snippet"]))
+    return True
+
+#get a channels' video ids, filter, and write to file
 if __name__ == "__main__":
-    #strips channel usernames from links in channels.txt.txt
-    channelList = stripUsers("test.txt")
+    store = []
+    channelList = stripUsers("channels.txt")
+    channelIds = multiChannels(channelList)
+    videoIds = getVideosFromPlaylist(channelIds)
+    i=1
+    for video in videoIds:
+        print(str(i)+"/"+str(len(videoIds)))
+        i += 1
+        if filterVideos(video, startdate="2024-01-01", enddate="2025-01-01"):
+            with open("channels-videos.txt", "a") as f:
+                f.write(video+"\n")
+
+#pull videos and comments from channel
+if __name__ == "__notmain__":
+    #strips channel usernames from links in file
+    channelList = stripUsers("joy.txt")
     #grabs channel ids from usernames
     channelIds = multiChannels(channelList)
     #gets all uploaded video id's from channel ID, vidNumber = number of uploads to grab
-    videoIds = getVideosFromPlaylist(channelIds,6)
+    videoIds = getVideosFromPlaylist(channelIds)
     for video in videoIds:
-        #singlevid filter should be in the format of a list ex ["replies","snippet"]
         #start/end time should be in the format year-mm-dd, undeclared, or empty string ""
+        if filterVideos(video, startdate="2024-01-01", enddate=""):
+        #singlevid filter should be in the format of a list ex ["replies","snippet"]
         #singlevid downloads video + comments, returns nothing if not between start and end
-        singleVid(video, filter=["replies","snippet"], startdate="", enddate="")
+            singleVid(video, filter=["replies","snippet"])
+
+#pull joy's videos
+if __name__ == "__notmain__":
+    channelList = stripUsers("joy.txt")
+    #grabs channel ids from usernames
+    channelIds = multiChannels(channelList)
+    #gets all uploaded video id's from channel ID, vidNumber = number of uploads to grab
+    videoIds = getVideosFromPlaylist(channelIds)
+    i=1
+    for video in videoIds:
+        #start/end time should be in the format year-mm-dd, undeclared, or empty string ""
+        filterVideos(video, startdate="2024-01-01", enddate="")
+        print(str(i) + "/" + str(len(videoIds)))
+        i+=1
