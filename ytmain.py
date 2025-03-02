31df8450b3ce1db73e9233bf0283bc5658aa185f
Sebastian <s.shing.smith@gmail.com>
2025-03-02 17:53:16 -0500

    got rid of unnecessary functions

--- ytmain.py
+++ ytmain.py
@@ -61,7 +61,7 @@ def singleVid(videoId, *args, **kwargs):
     else:
         with open(cleanedFilename, "w") as f:
             f.write(json.dumps(response))
-
+#gets uploads playlist ids
 def multiChannels(channelNames):
     youtube = apiAccess()
     ret = {}
@@ -94,6 +94,7 @@ def getVideosFromPlaylist(channelIds):
         print(channelId + " - " +str(i)+ "/"+ str(len(channelIds)) )
         i+=1
         nextPageToken = ""
+        i2 = 0
         while nextPageToken is not None:
             if nextPageToken == "":
                 nextPageToken = None
@@ -104,10 +105,14 @@ def getVideosFromPlaylist(channelIds):
                 pageToken=nextPageToken,
                 playlistId=channelIds[channelId]
             )
-            response = request.execute()
-            for item in response['items']:
-                ret.append(item['contentDetails']['videoId'])
-            nextPageToken = response.get('nextPageToken')
+            try:
+                response = request.execute()
+                for item in response['items']:
+                    ret.append(item['contentDetails']['videoId'])
+            except TimeoutError as e:
+                print(e)
+            finally:
+                nextPageToken = response.get('nextPageToken')
     return ret
 
 def filterVideos(videoId, *args, **kwargs):
@@ -129,26 +134,28 @@ def filterVideos(videoId, *args, **kwargs):
         enddate = date.fromisoformat(enddate)
         if datePublished > enddate:
             return False
-    with open("joy-info.txt", "a") as f:
-        f.write(json.dumps(info["items"][0]["snippet"]))
+   # with open("joy-info.txt", "a") as f:
+        #f.write(json.dumps(info["items"][0]["snippet"]))
     return True
 
-#get a channels' video ids, filter, and write to file
-if __name__ == "__main__":
-    store = []
-    channelList = stripUsers("channels.txt")
-    channelIds = multiChannels(channelList)
-    videoIds = getVideosFromPlaylist(channelIds)
-    i=1
-    for video in videoIds:
-        print(str(i)+"/"+str(len(videoIds)))
-        i += 1
-        if filterVideos(video, startdate="2024-01-01", enddate="2025-01-01"):
-            with open("channels-videos.txt", "a") as f:
-                f.write(video+"\n")
+def countVideos(filename):
+    with open(filename, "r") as f:
+        channel =""
+        videoCount = 0
+        for line in f:
+            if len(line)> 20:
+                print("channel:",channel)
+                print("videocount:",videoCount)
+                videoCount = 0
+                channel =line
+            else:
+                videoCount +=1
+        print("channel:", channel)
+        print("videocount:", videoCount)
+
 
 #pull videos and comments from channel
-if __name__ == "__notmain__":
+if __name__ == "__main__":
     #strips channel usernames from links in file
     channelList = stripUsers("joy.txt")
     #grabs channel ids from usernames
@@ -161,17 +168,3 @@ if __name__ == "__notmain__":
         #singlevid filter should be in the format of a list ex ["replies","snippet"]
         #singlevid downloads video + comments, returns nothing if not between start and end
             singleVid(video, filter=["replies","snippet"])
-
-#pull joy's videos
-if __name__ == "__notmain__":
-    channelList = stripUsers("joy.txt")
-    #grabs channel ids from usernames
-    channelIds = multiChannels(channelList)
-    #gets all uploaded video id's from channel ID, vidNumber = number of uploads to grab
-    videoIds = getVideosFromPlaylist(channelIds)
-    i=1
-    for video in videoIds:
-        #start/end time should be in the format year-mm-dd, undeclared, or empty string ""
-        filterVideos(video, startdate="2024-01-01", enddate="")
-        print(str(i) + "/" + str(len(videoIds)))
-        i+=1
